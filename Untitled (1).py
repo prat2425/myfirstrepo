857c8a1bb338e6428f56ec0059295abb7bd144e0
prat2425 <92031235+prat2425@users.noreply.github.com>
2022-12-02 20:32:42 +0530

    Add files via upload

---
+++ Untitled (1).py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+
+
+# In[2]:
+
+
+print(a)
+
+
+# In[3]:
+
+
+print(a)
+
+
+# In[4]:
+
+
+import os
+
+
+# In[5]:
+
+
+os.getcwd()
+
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[9]:
+
+
+pd.read_csv("pt.tsv",sep="/t")
+
+
+# In[ ]:
+
+
+
+
